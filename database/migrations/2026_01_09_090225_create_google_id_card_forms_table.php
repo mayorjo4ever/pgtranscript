b6191c113b098507457d1e54c453c2f473992455
Isaac Mayowa OJO <mayorjo4ever@gmail.com>
2026-01-09 15:48:52 +0100

    Google ID Card
    Google ID Card

---
+++ database/migrations/2026_01_09_090225_create_google_id_card_forms_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('google_id_card_forms', function (Blueprint $table) {
+            $table->id();
+            $table->datetime('request_time'); 
+            $table->string('request_status')->default('applied');
+            $table->string('request_email'); 
+            $table->string('regno');
+            $table->string('fullname');
+            $table->string('phone')->nullable();
+            $table->string('entry_session')->nullable();
+            $table->string('degree')->nullable();
+            $table->string('programme')->nullable();
+            $table->string('faculty')->nullable();
+            $table->string('department')->nullable();
+            $table->text('passport');
+            $table->text('signature');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('google_id_card_forms');
+    }
+};
