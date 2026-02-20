fe18efd4e26bb482acb64a11e4ca0c4e79ac86b9
Isaac Mayowa OJO <mayorjo4ever@gmail.com>
2026-02-20 08:41:25 +0100

    login control
    login control

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return redirect('portal/login');
 });
 
+ Route::get('login',function () {
+    return redirect('portal/login');
+});
+
 
 ## login page
 ##===================
