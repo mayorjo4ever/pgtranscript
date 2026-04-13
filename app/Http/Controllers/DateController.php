1571bf4569f2510bfeea9811baf8ba9b0559492c
Isaac Mayowa OJO <mayorjo4ever@gmail.com>
2026-04-13 20:49:23 +0100

    updates
    updates

--- app/Http/Controllers/DateController.php
+++ app/Http/Controllers/DateController.php
@@ -8,6 +8,7 @@ class DateController extends Controller
 {
     public function index()
     {
-        return view('front.date.index');
+        $page_info = ['title'=> "Date Conversion Made Easy",'icon'=>'pe-7s-person_add','sub-title'=>'We Love Simplicity'];
+        return view('front.date.index',compact('page_info'));
     }
 }
