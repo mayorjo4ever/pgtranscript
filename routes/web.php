cd7b40d30f944d14070b9de9b433dea6bb54d11a
Isaac Mayowa OJO <mayorjo4ever@gmail.com>
2026-04-24 10:28:11 +0100

    updates
    updates

--- routes/web.php
+++ routes/web.php
@@ -189,7 +189,10 @@ Route::prefix('/portal')->namespace('App\Http\Controllers\Portal')->group(functi
         # $report = TranscriptReport::where('regno','05/66MF075')->get();             
         
         //$data = CertificateData::where('regno','11/66MC067')->get(); 
-
+     CertificateData::whereIn('id',[8351,8350,8353,8352])
+                ->where('approve_date_id',280)
+                ->delete(); 
+                
         $data = CertificateData::whereIn('regno',['14/56ED115',
                     '12/67QV020',
                     '21/68OJ013',                    
@@ -198,6 +201,7 @@ Route::prefix('/portal')->namespace('App\Http\Controllers\Portal')->group(functi
                 ->where('approve_date_id',280)
                 ->get();         
         
+
         // $data->update(['approve_date_id'=>104,'year'=>'2014']);
 
         print "<pre>"; 
