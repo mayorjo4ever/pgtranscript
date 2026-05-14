56ab0909571f111dd1d60fda65318f0d864b0ab5
Isaac Mayowa OJO <mayorjo4ever@gmail.com>
2026-05-14 10:12:49 +0100

    updates
    updates

--- app/Http/Controllers/Admin/TranscriptRequestController.php
+++ app/Http/Controllers/Admin/TranscriptRequestController.php
@@ -711,7 +711,8 @@ class TranscriptRequestController extends Controller
           *  <a href="{{url('admin/print-phd-transcript/'.$url)}}" target="_blank" class="btn btn-primary "> PRINT {{ $printout->type.' Transcript ' }}   [ {{ $printout->print_count }} ]</a>
           *     <!-- include cover letter -->               
         */
-         $printout_url = 'admin/print-phd-transcript/' . base64_encode($request->regno."|".$request->approve_date."|".$pid->id); 
+         # $printout_url = 'admin/print-phd-transcript/' . base64_encode($request->regno."|".$request->approve_date."|".$pid->id); 
+         $printout_url = 'admin/print-phd-transcript/' . base64_encode($request->regno."|".$pid->id); 
          
          TranscriptsRequest::where('id',$request->request_id)
                 ->update(['request_status'=>'Treated','transcript_cover_letter_id'=>$coverLetter->id,
